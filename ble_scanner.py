ba6562cabeeb368f3afa3a10e4eb53b68c8b3a07
Zsoci95 <el19zk@leeds.ac.uk>
2023-03-03 14:36:59 +0000

    File renamed and added scanner

--- ble_scanner.py
+++ ble_scanner.py
@@ -1,55 +1,13 @@
-#!/usr/bin/env python3
-import asyncio
-from bleak import BleakClient, BleakError 
-import socket
-import platform 
-
-os_name = platform.system() 
-if os_name == 'Windows':
-    DEVICE_ADDRESS = "78:21:84:8A:14:F2" 
-elif os_name == 'Darwin':
-    DEVICE_ADDRESS = "F5A6EF50-BF44-B5DB-3BD0-0432180F23FF"
+import bleak 
+import asyncio 
 
 
-# UUIDs for the service and characteristics that contain the data
 
-SERVICE_UUID = "bd0f56c6-a403-4d3a-86ba-6fed11ce8473"
-CHARACTERISTIC_UUID = "1fe90638-437c-490c-ad92-bda3b9423bab"
-UDP_PORT = 12345
-UDP_ADDRESS = '127.0.0.1'
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#sock.bind((UDP_ADDRESS, UDP_PORT))
-
-async def data_handler(data): 
-    try:
-        data_str = data.decode('utf-8')
-        sock.sendto(bytes(data_str, "utf-8"), (UDP_ADDRESS, UDP_PORT))
-        data_list = data_str.split(',')
-        print(data_str)
-    except UnicodeDecodeError:
-        print("UnicodeDecodeError")
-    
-    
-   
+async def run(): 
+    devices = await bleak.BleakScanner.discover()
+    for d in devices:
+        print(d)
 
 
-async def run():
-    client = BleakClient(DEVICE_ADDRESS)
-    try:
-        await client.connect()
-        while True:
-            data = await client.read_gatt_char(CHARACTERISTIC_UUID) 
-            await data_handler(data)
-    except BleakError as e: 
-        print(e)
-    
-
-
-    
-
-
-# Run the coroutine in the event loop
-loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop() 
 loop.run_until_complete(run())
-
-
